fc714f2265489a417a50a98bc35173825d4d21c9
Freddy Cansick <freddycansick@gmail.com>
2024-05-15 17:30:45 +0100

    Updated build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let nix_ld_library_path = std::env::var("NIX_LD_LIBRARY_PATH");
-    if let Ok(path) = nix_ld_library_path {
-        #[cfg(target_os = "linux")]
-        println!("cargo:rustc-link-search={}", path);
+    #[cfg(target_os = "linux")]
+    {
+        let nix_ld_library_path = std::env::var("NIX_LD_LIBRARY_PATH");
+        if let Ok(path) = nix_ld_library_path {
+            println!("cargo:rustc-link-search={}", path);
+        }
     }
 }
