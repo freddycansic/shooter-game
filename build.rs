d91dccd6c437bf5b94083674005b7a920bccfbbf
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2025-12-28 18:27:10 +0000

    chore: remove unrequired build.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    #[cfg(target_os = "linux")]
-    {
-        let nix_ld_library_path = std::env::var("NIX_LD_LIBRARY_PATH");
-        if let Ok(path) = nix_ld_library_path {
-            println!("cargo:rustc-link-search={}", path);
-        }
-    }
-}
