263faa21c7f19b4d9dd6ec8c2980c35678f23869
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2026-01-24 16:19:10 +0000

    feat: capsule-aabb intersection + tests

--- common/src/collision/colliders/aabb.rs
+++ common/src/collision/colliders/aabb.rs
@@ -1,9 +1,10 @@
-use nalgebra::Point3;
+use nalgebra::{Point3, Vector3};
 
 use crate::{
     collision::collidable::{RayHit, Intersectable},
     maths::Ray,
 };
+use crate::collision::colliders::capsule::Capsule;
 
 #[derive(Debug)]
 pub struct Aabb {
@@ -26,9 +27,54 @@ impl Aabb {
             ),
         }
     }
+
+    fn intersects_capsule_t(&self, t: f32, capsule: &Capsule, ba: &Vector3<f32>) -> bool {
+        let p_t = capsule.p1 + t * ba;
+
+        let pb_t = Vector3::new(
+            p_t.x.clamp(self.min.x, self.max.x),
+            p_t.y.clamp(self.min.y, self.max.y),
+            p_t.z.clamp(self.min.z, self.max.z)
+        );
+
+        (pb_t - p_t).magnitude_squared() <= capsule.radius * capsule.radius
+    }
 }
 
 impl Intersectable for Aabb {
+    fn intersects_capsule(&self, capsule: &Capsule) -> bool {
+        let ba = capsule.p2 - capsule.p1;
+
+        // Test endpoints
+        for end in [0.0, 1.0] {
+            if self.intersects_capsule_t(end, capsule, &ba) {
+                return true;
+            }
+        }
+
+        // Test min max of each slab
+        for i in 0..=2 {
+            for point in [self.min, self.max] {
+                if ba[i] == 0.0 {
+                    continue;
+                }
+
+                let t = (point[i] - capsule.p1[i]) / ba[i];
+
+                // Only test values within the line segment
+                if t < 0.0 || t > 1.0 {
+                    continue;
+                }
+
+                if self.intersects_capsule_t(t, capsule, &ba) {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
     fn intersect_ray(&self, ray: &Ray) -> Option<RayHit> {
         let mut tmin = f32::NEG_INFINITY; // earliest possible intersection
         let mut tmax = f32::INFINITY; // latest possible intersection
@@ -151,5 +197,175 @@ mod tests {
 
         let result = aabb.intersect_ray(&ray).unwrap();
         assert_relative_eq!(result.tmin, 1.0);
+    }
+
+    // -----------------------------
+
+    #[test]
+    fn intersect_capsule_aabb_face_hit_capsule_segment() {
+        let capsule = Capsule::new(Vector3::new(0.0, 1.5, 0.0), Vector3::new(0.0, -0.5, 0.0), 1.0);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_face_graze_capsule_end() {
+        let capsule = Capsule::new(Vector3::new(0.0, 2.5, 0.0), Vector3::new(0.0, 2.0, 0.0), 1.0);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_face_barely_miss_capsule_end() {
+        let capsule = Capsule::new(Vector3::new(0.0, 2.5, 0.0), Vector3::new(0.0, 2.0, 0.0), 0.99);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), false);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_face_intersect_capsule_end() {
+        let capsule = Capsule::new(Vector3::new(0.0, 2.5, 0.0), Vector3::new(0.0, 2.0, 0.0), 1.5);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_face_miss_capsule_end() {
+        let capsule = Capsule::new(Vector3::new(0.0, 7.5, 0.0), Vector3::new(0.0, 5.0, 0.0), 1.5);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), false);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_corner_intersect_capsule_segment() {
+        let capsule = Capsule::new(Vector3::new(5.0, 5.0, 5.0), Vector3::new(0.0, 0.0, 0.0), 1.0);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_corner_graze_capsule_end() {
+        let capsule = Capsule::new(Vector3::new(5.0, 5.0, 5.0), Vector3::new(2.0, 2.0, 2.0), 3.0_f32.sqrt());
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_corner_miss_capsule() {
+        let capsule = Capsule::new(Vector3::new(5.0, 5.0, 5.0), Vector3::new(2.0, 2.0, 2.0), 1.0);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), false);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_edge_hit_capsule_segment() {
+        let capsule = Capsule::new(Vector3::new(-2.0, -2.0, 0.0), Vector3::new(-2.0, 2.0, 0.0), 1.2);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_edge_graze_capsule_segment() {
+        let capsule = Capsule::new(Vector3::new(-2.0, -2.0, 0.0), Vector3::new(-2.0, 2.0, 0.0), 1.0);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_edge_barely_miss_capsule() {
+        let capsule = Capsule::new(Vector3::new(-2.0, -2.0, 0.0), Vector3::new(-2.0, 2.0, 0.0), 0.99);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), false);
+    }
+
+    #[test]
+    fn intersect_capsule_capsule_inside_aabb() {
+        let capsule = Capsule::new(Vector3::new(0.0, -0.1, 0.0), Vector3::new(0.0, 0.1, 0.0), 0.5);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_aabb_inside_capsule() {
+        let capsule = Capsule::new(Vector3::new(0.0, -0.1, 0.0), Vector3::new(0.0, 0.1, 0.0), 50.0);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
+    }
+
+    #[test]
+    fn intersect_capsule_capsule_length_zero() {
+        let capsule = Capsule::new(Vector3::new(0.0, 1.0, 0.0), Vector3::new(0.0, 1.0, 0.0), 1.0);
+
+        let aabb = Aabb {
+            min: Point3::new(-1.0, -1.0, -1.0),
+            max: Point3::new(1.0, 1.0, 1.0),
+        };
+
+        assert_eq!(aabb.intersects_capsule(&capsule), true);
     }
 }
