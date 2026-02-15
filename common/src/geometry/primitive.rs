70cc9b863f7d2cf71b7307c9752c0830fc0bcada
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2026-02-15 17:13:14 +0000

    feat: add option to make primitives without gpu buffers

--- common/src/geometry/primitive.rs
+++ common/src/geometry/primitive.rs
@@ -4,8 +4,18 @@ use super::geometry_vertex::GeometryVertex;
 
 #[derive(Debug)]
 pub struct Primitive {
-    pub vertex_buffer: VertexBuffer<GeometryVertex>,
-    pub index_buffer: IndexBuffer<u32>,
+    pub cpu: PrimitiveCpu,
+    pub gpu: Option<PrimitiveGpu>,
+}
+
+#[derive(Debug)]
+pub struct PrimitiveCpu {
     pub vertices: Vec<GeometryVertex>,
     pub indices: Vec<u32>,
+}
+
+#[derive(Debug)]
+pub struct PrimitiveGpu {
+    pub vertex_buffer: VertexBuffer<GeometryVertex>,
+    pub index_buffer: IndexBuffer<u32>,
 }
