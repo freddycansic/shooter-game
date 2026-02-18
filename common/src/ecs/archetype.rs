8a1dc65cd80a90c1b37311f373b97f398121d4e1
Freddy Cansick <93549743+freddycansic@users.noreply.github.com>
2026-02-18 21:58:54 +0000

    refactor: simplify archetype storage

--- common/src/ecs/archetype.rs
+++ common/src/ecs/archetype.rs
@@ -2,36 +2,21 @@ use crate::ecs::component::Component;
 use crate::ecs::entity::Entity;
 use std::any::Any;
 
-trait ColumnTrait: Any {
-    fn as_any(&self) -> &dyn Any;
-}
-
-// Essentially a type erased Vec<T>
-// 'static here means that T does not contain any non 'static references
-// Any requires 'static because the type it will be cast into must be independent of references
-struct Column<T: Component + 'static>(pub Vec<T>);
-
-impl<T: Component> ColumnTrait for Column<T> {
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-}
-
 pub struct Archetype {
     id: u32,
     entities: Vec<Entity>,
-    components: Vec<(u32, Box<dyn ColumnTrait>)>,
+    // Any here must be Vec<T: Component>
+    components: Vec<(u32, Box<dyn Any>)>,
 }
 
 impl Archetype {
+    // TODO pub fn push_entity
+    
     pub fn components<T: Component + 'static>(&self) -> Option<&[T]> {
         let id = T::id();
 
-        let (_, column) = self.components.iter().find(|(component_id, c)| *component_id == id)?;
+        let (_, column) = self.components.iter().find(|(component_id, _)| *component_id == id)?;
 
-        column
-            .as_any()
-            .downcast_ref::<Column<T>>()
-            .map(|data| data.0.as_slice())
+        column.downcast_ref::<Vec<T>>().map(|data| data.as_slice())
     }
 }
